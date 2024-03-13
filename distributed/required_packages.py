b0a72e5420c9ac7fca562dae27a17fa1e51413a0
ChongzhouFang <chongzhoufang@outlook.com>
2024-03-13 14:51:23 -0700

    update required_packages.py

--- distributed/required_packages.py
+++ distributed/required_packages.py
@@ -1,8 +1,8 @@
 required_packages = {
     'chameleon': ['numpy', 'matplotlib', 'pandas'],
     'floatoperation': ['requests', 'beautifulsoup4', 'flask'],
-    'jsondumpsloads':['scikit-learn', 'tensorflow', 'keras'],
-    'linpack':['django', 'sqlalchemy', 'pytest'],
+    'jsondumpsloads':['scikit-learn', 'tensorflow', 'keras', 'numpy', 'requests', 'flask'],
+    'linpack':['django', 'sqlalchemy', 'pytest', 'numpy', 'matplotlib', 'pandas', 'beautifulsoup4', 'flask'],
     'matmul':['selenium', 'pyautogui', 'pyperclip'],
     'pyaes':['pytorch', 'torchvision', 'numpy'],
     'imageprocessing':['networkx', 'matplotlib', 'scipy'],
