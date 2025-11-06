d28900fdfe1752af79b643c1732ae40a38a7efb7
Chongzhou Fang <cxfeec@kgcoe-ce2603.ad.rit.edu>
2025-11-06 10:17:36 -0500

    update test

--- test-dask.py
+++ test-dask.py
@@ -34,10 +34,24 @@ def linpack4(data):
         data
     )
 
+def linpack5(data):
+    print(
+        "Linpack!",
+        data
+    )
+
+def linpack6(data):
+    print(
+        "Linpack!",
+        data
+    )
+
 if __name__ == '__main__':
-    # c = distributed.Client('tcp://129.21.123.64:8786') # Chongzhou
-    c = distributed.Client('tcp://172.31.246.35:8786') # Wei
+    c = distributed.Client('tcp://129.21.123.64:8786') # Chongzhou
+    # c = distributed.Client('tcp://172.31.246.35:8786') # Wei
     futures = c.submit(linpack, random.randint(1,10000), user_id="bob124")
     futures = c.submit(linpack2, random.randint(1,10000), user_id="bob124")
     futures = c.submit(linpack3, random.randint(1,10000), user_id="bob124")
     futures = c.submit(linpack4, random.randint(1,10000), user_id="bob125")
+    futures = c.submit(linpack5, random.randint(1,10000), user_id="bob126")
+    futures = c.submit(linpack6, random.randint(1,10000), user_id="bob127")
